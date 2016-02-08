07581aba63c8c7d1a3864711f6e464884b2d13e2
Mike Galvin <galvi024@umn.edu>
2016-02-08 15:38:22 -0600

    merging

--- src/test/groovy/seng5199/twtr/AccountSpec.groovy
+++ src/test/groovy/seng5199/twtr/AccountSpec.groovy
@@ -13,14 +13,49 @@ class AccountSpec extends Specification {
 
     }
 
-    void "Account saves name"() {
-        when:
-        def name = new Account(handle: 'mikeg008', email: 'galvi024@umn.edu', password: 123456789, name: 'Mike')
+    void "User enters everything correctly"() {
+        when: 'should pass when all parameters are created as expected'
+        def account = new Account(handle:'', email: '', password: '', name: '')
 
         then:
-        name.validate()
+        result == account.validate()
+
+        where:
+        handle          |   email                   |   password        |  name         |   result
+        'mike'          |   'galvi024@gmail.com'    |   '1234567aH'     |   'Mike'      |   true
+        'mike2'         |   'galvi024@umn.edu'      |   'fdgd'          |   'Mike'      |   false
+
     }
 
+//    void "Handle is blank"() {
+//        when: 'should fail when handle is blank'
+//        def name = new Account(handle: '', email: 'galvi024@umn.edu', password: 123456789, name: 'Mike')
+//
+//        then: 'validation should fail'
+//        !name.validate()
+//    }
+//
+//    void "Email is blank"() {
+//        when: 'should fail when email is blank'
+//        def name = new Account(handle: 'mikeg008', email: '', password: 123456789, name: 'Mike')
+//
+//        then: 'validation should fail'
+//        !name.validate()
+//    }
+//    void "Password is blank"() {
+//        when: 'should fail when password is blank'
+//        def name = new Account(handle: 'mikeg008', email: 'galvi024@umn.edu', password: '', name: 'Mike')
+//
+//        then: 'validation should fail'
+//        !name.validate()
+//    }
+//    void "Name is blank"() {
+//        when: 'should fail when name is blank'
+//        def name = new Account(handle: 'mikeg008', email: 'galvi024@umn.edu', password: 123456789, name: '')
+//
+//        then: 'validation should fail'
+//        !name.validate()
+//    }
     def cleanup() {
     }
 
